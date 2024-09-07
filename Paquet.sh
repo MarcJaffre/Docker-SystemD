55d02eaaa1b359166612d5372800116bfbf5becc
Marc <teste74@hotmail.fr>
2024-09-07 11:49:25 +0200

    Update Paquet.sh

--- Paquet.sh
+++ Paquet.sh
@@ -12,8 +12,15 @@ apt install -y openssh-server;
 apt install -y python3-websockify
 
 ###############################################################################################################################################################################################################################
-# Configuration #
-#################
+# Configuration SSH #
+#####################
 sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config 2>/dev/null;
 
+
+###############################################################################################################################################################################################################################
+# OpenSSL #
+###########
+mdkir -p /usr/share/novnc/;
+(echo "FR"; echo "France"; echo "Paris"; echo "Personnel"; echo "Personnel"; echo "$(hostname)"; echo "mail@exemple.co"; ) | \
+openssl req -x509 -nodes -newkey rsa:3072 -keyout /usr/share/novnc/novnc.pem -out /usr/share/novnc/novnc.pem -days 3650
 
