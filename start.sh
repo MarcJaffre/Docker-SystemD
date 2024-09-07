f4f56a011ca530bbfa13f280cdab21b38f7e619d
Marc <teste74@hotmail.fr>
2024-09-07 14:59:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,8 @@
 ###############################################################################################################################################################################################################################
 # Gestion des services #
 ########################
-systemctl enable --now novnc;
-systemctl enable --now ssh;
-
+#systemctl enable --now novnc;
+#systemctl enable --now ssh;
 
 # Desktop Requis !
-systemctl enable --now tigervncserver;
+#systemctl enable --now tigervncserver;
