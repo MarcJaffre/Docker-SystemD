f92eca473c0046a6a48cfd32c709442709b6ef03
Marc <teste74@hotmail.fr>
2024-09-07 12:24:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,5 @@ systemctl disable --now ssh   & 2>/dev/null;
 # Environnement Graphique #
 ###########################
 /usr/bin/tigervncserver -xstartup /usr/bin/mate-session -autokill -localhost no :1
+
+#  -SecurityTypes None
