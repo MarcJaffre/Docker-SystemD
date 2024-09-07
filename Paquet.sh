106eca949fe8c9763947735248d2865029841cbc
Marc <teste74@hotmail.fr>
2024-09-07 13:49:53 +0200

    Update Paquet.sh

--- Paquet.sh
+++ Paquet.sh
@@ -29,8 +29,6 @@ sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh
 #(echo "FR"; echo "France"; echo "Paris"; echo "Personnel"; echo "Personnel"; echo "$(hostname)"; echo "mail@exemple.co"; ) | \
 #openssl req -x509 -nodes -newkey rsa:3072 -keyout /usr/share/novnc/novnc.pem -out /usr/share/novnc/novnc.pem -days 3650
 
-
-
 ###############################################################################################################################################################################################################################
 # Environnement User #
 ######################
