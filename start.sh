1c538b1522ae0e9d03ed8033a5c832562d947fa3
Marc <teste74@hotmail.fr>
2024-09-07 10:21:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 # Load systemd daemon
-/sbin/init
+systemd --system=/sysroot
+
+# Enable and start the NoVNC service
+systemctl enable novnc.service
+systemctl start novnc.service
+
+# Enable and start the Apache service
+systemctl enable apache2.service
+systemctl start apache2.service
+
+# Enable and start the MySQL service
+systemctl enable mysql.service
+systemctl start mysql.service
+
+# Enable and start the SSH service
+systemctl enable ssh.service
+systemctl start ssh.service
+
+# Check the status of all services
+systemctl list-units --type=service
+
+
+
+
 
 # Disable and start the SSH service
 #sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
