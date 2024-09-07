d83d55218771544e0dc857ef6166753d5a626310
Marc <teste74@hotmail.fr>
2024-09-07 11:08:19 +0200

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 
-
-
-RUN sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
-RUN sed -i -e "s/^#Port 22/Port 222/g" /etc/ssh/sshd_config;
-
+sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+sed -i -e "s/^#Port 22/Port 222/g" /etc/ssh/sshd_config;
 
 systemctl stop ssh
