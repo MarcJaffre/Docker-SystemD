6fdc90ae49e226dce1109b8de8914f42bca0e6e3
Marc <teste74@hotmail.fr>
2024-09-07 20:36:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 ###################################################################
 # Demarrage des services #
 ##########################
-systemctl start ssh;
-systemctl start novnc;
-systemctl start tigervnc;
+#systemctl start ssh;
+#systemctl start novnc;
+#systemctl start tigervnc;
+
+/usr/bin/tigervncserver -localhost :99 -xstartup /etc/X11/Xfce4/xinitrc &
