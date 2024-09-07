8cfb63c1ca640c4c79ec93d2a0cc0a5b506ccbee
Marc <teste74@hotmail.fr>
2024-09-07 12:27:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
-
 ##########################################################################################################################################################
-# Environnement Graphique #
-###########################
+# Bureautique #
+###############
 /usr/bin/tigervncserver -xstartup /usr/bin/mate-session -autokill -localhost no :1
 
 #  -SecurityTypes None
