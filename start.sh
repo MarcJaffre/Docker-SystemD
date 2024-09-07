4257c919986d61a799b0cae9664cc5b948d85e1a
Marc <teste74@hotmail.fr>
2024-09-07 20:40:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,7 @@
 ###################################################################
 # Demarrage des services #
 ##########################
-#systemctl start ssh;
-#systemctl start novnc;
-#systemctl start tigervnc;
+systemctl start ssh;
+systemctl start novnc;
+systemctl start tigervnc;
 
-/usr/bin/tigervncserver -localhost :99 -xstartup /etc/X11/Xfce4/xinitrc &
-/usr/bin/websockify -D --web /usr/share/novnc/ 6080 localhost:5900 &
