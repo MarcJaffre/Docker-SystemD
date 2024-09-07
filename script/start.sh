ae69ccceff20d0e79e972c77d9e984c736dea874
Marc <teste74@hotmail.fr>
2024-09-07 11:18:06 +0200

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
-sed -i -e "s/^#Port 22/Port 222/g" /etc/ssh/sshd_config;
+##########################################################################################################################################################
+# Configuration de NOVNC #
+##########################
+systemctl enable novnc;
 
-systemctl stop ssh
+##########################################################################################################################################################
+# Configuration du SSH #
+########################
+sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+systemctl disable ssh;
