9edbd950721d7ed5bf251c4677cf873d187171de
Marc <teste74@hotmail.fr>
2024-09-07 10:24:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,24 +5,9 @@
 # Load systemd daemon
 systemd --system=/sysroot
 
-# Enable and start the NoVNC service
-systemctl enable novnc.service
-systemctl start novnc.service
-
-# Enable and start the Apache service
-systemctl enable apache2.service
-systemctl start apache2.service
-
-# Enable and start the MySQL service
-systemctl enable mysql.service
-systemctl start mysql.service
-
 # Enable and start the SSH service
 systemctl enable ssh.service
 systemctl start ssh.service
-
-# Check the status of all services
-systemctl list-units --type=service
 
 
 
