1de525ad11adf905fdd30d144a1783bd702317df
Marc <teste74@hotmail.fr>
2024-09-07 10:15:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,10 @@
 systemd --system=/sysroot
 
 # Disable and start the SSH service
-sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
-sed -i -e "s/^#Port 22/Port 222/g" /etc/ssh/sshd_config;
-
-systemctl disable ssh.service
-systemctl stop ssh.service
+#sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+#sed -i -e "s/^#Port 22/Port 222/g" /etc/ssh/sshd_config;
+#systemctl disable ssh.service
+#systemctl stop ssh.service
 
 # Check the status of all services
-systemctl list-units --type=service
+#systemctl list-units --type=service
